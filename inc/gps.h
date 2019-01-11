b388cbef1a76aed2992087ff19cf6301d304418d
Jacob Rodgers <jacob.luojie@gmail.com>
2019-01-11 00:02:32 -0500

    added newlines to ends of GPS commands, added functionality for getting time from GPS

--- inc/gps.h
+++ inc/gps.h
@@ -1,6 +1,8 @@
 #ifndef GPS_H
 #define GPS_H
 
+#include <time.h>
+
 uint8_t GPS_Initialize();
 
 uint8_t GPS_CheckForNewData();
@@ -16,5 +18,9 @@ uint8_t GPS_GetAltitude(float *altitude, char *altitude_units);
 uint8_t GPS_GetGroundSpeedKnots(float *gs_knots);
 
 uint8_t GPS_GetNumSats(int *num_sats);
+
+uint8_t GPS_GetDateTime(int *year, int *month, int *day, int *hours, int *minutes, int *seconds);
+
+uint8_t GPS_GetUNIXTimestamp(struct timespec *ts);
 
 #endif
--- src/gps.c
+++ src/gps.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <time.h>
 
 #include "stm32f4xx.h"
 #include "FreeRTOS.h"
@@ -20,57 +21,57 @@
 // different commands to set the update rate from once a second (1 Hz) to 10 times a second (10Hz)
 // Note that these only control the rate at which the position is echoed, to actually speed up the
 // position fix you must also send one of the position fix rate commands below too.
-#define PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ  "$PMTK220,10000*2F" // Once every 10 seconds, 100 millihertz.
-#define PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ  "$PMTK220,5000*1B"  // Once every 5 seconds, 200 millihertz.
-#define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F"
-#define PMTK_SET_NMEA_UPDATE_2HZ  "$PMTK220,500*2B"
-#define PMTK_SET_NMEA_UPDATE_5HZ  "$PMTK220,200*2C"
-#define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F"
+#define PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ  "$PMTK220,10000*2F\r\n" // Once every 10 seconds, 100 millihertz.
+#define PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ  "$PMTK220,5000*1B\r\n"  // Once every 5 seconds, 200 millihertz.
+#define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F\r\n"
+#define PMTK_SET_NMEA_UPDATE_2HZ  "$PMTK220,500*2B\r\n"
+#define PMTK_SET_NMEA_UPDATE_5HZ  "$PMTK220,200*2C\r\n"
+#define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F\r\n"
 // Position fix update rate commands.
-#define PMTK_API_SET_FIX_CTL_100_MILLIHERTZ  "$PMTK300,10000,0,0,0,0*2C" // Once every 10 seconds, 100 millihertz.
-#define PMTK_API_SET_FIX_CTL_200_MILLIHERTZ  "$PMTK300,5000,0,0,0,0*18"  // Once every 5 seconds, 200 millihertz.
-#define PMTK_API_SET_FIX_CTL_1HZ  "$PMTK300,1000,0,0,0,0*1C"
-#define PMTK_API_SET_FIX_CTL_5HZ  "$PMTK300,200,0,0,0,0*2F"
+#define PMTK_API_SET_FIX_CTL_100_MILLIHERTZ  "$PMTK300,10000,0,0,0,0*2C\r\n" // Once every 10 seconds, 100 millihertz.
+#define PMTK_API_SET_FIX_CTL_200_MILLIHERTZ  "$PMTK300,5000,0,0,0,0*18\r\n"  // Once every 5 seconds, 200 millihertz.
+#define PMTK_API_SET_FIX_CTL_1HZ  "$PMTK300,1000,0,0,0,0*1C\r\n"
+#define PMTK_API_SET_FIX_CTL_5HZ  "$PMTK300,200,0,0,0,0*2F\r\n"
 // Can't fix position faster than 5 times a second!
 
 
-#define PMTK_SET_BAUD_57600 "$PMTK251,57600*2C"
-#define PMTK_SET_BAUD_9600 "$PMTK251,9600*17"
+#define PMTK_SET_BAUD_57600 "$PMTK251,57600*2C\r\n"
+#define PMTK_SET_BAUD_9600 "$PMTK251,9600*17\r\n"
 
 // turn on only the second sentence (GPRMC)
-#define PMTK_SET_NMEA_OUTPUT_RMCONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
+#define PMTK_SET_NMEA_OUTPUT_RMCONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"
 // turn on GPRMC and GGA
-#define PMTK_SET_NMEA_OUTPUT_RMCGGA "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+#define PMTK_SET_NMEA_OUTPUT_RMCGGA "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"
 // turn on ALL THE DATA
-#define PMTK_SET_NMEA_OUTPUT_ALLDATA "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+#define PMTK_SET_NMEA_OUTPUT_ALLDATA "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"
 // turn off output
-#define PMTK_SET_NMEA_OUTPUT_OFF "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+#define PMTK_SET_NMEA_OUTPUT_OFF "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"
 
 // to generate your own sentences, check out the MTK command datasheet and use a checksum calculator
 // such as the awesome http://www.hhhh.org/wiml/proj/nmeaxor.html
 
-#define PMTK_LOCUS_STARTLOG  "$PMTK185,0*22"
-#define PMTK_LOCUS_STOPLOG "$PMTK185,1*23"
-#define PMTK_LOCUS_STARTSTOPACK "$PMTK001,185,3*3C"
-#define PMTK_LOCUS_QUERY_STATUS "$PMTK183*38"
-#define PMTK_LOCUS_ERASE_FLASH "$PMTK184,1*22"
+#define PMTK_LOCUS_STARTLOG  "$PMTK185,0*22\r\n"
+#define PMTK_LOCUS_STOPLOG "$PMTK185,1*23\r\n"
+#define PMTK_LOCUS_STARTSTOPACK "$PMTK001,185,3*3C\r\n"
+#define PMTK_LOCUS_QUERY_STATUS "$PMTK183*38\r\n"
+#define PMTK_LOCUS_ERASE_FLASH "$PMTK184,1*22\r\n"
 #define LOCUS_OVERLAP 0
 #define LOCUS_FULLSTOP 1
 
-#define PMTK_ENABLE_SBAS "$PMTK313,1*2E"
-#define PMTK_ENABLE_WAAS "$PMTK301,2*2E"
+#define PMTK_ENABLE_SBAS "$PMTK313,1*2E\r\n"
+#define PMTK_ENABLE_WAAS "$PMTK301,2*2E\r\n"
 
 // standby command & boot successful message
-#define PMTK_STANDBY "$PMTK161,0*28"
-#define PMTK_STANDBY_SUCCESS "$PMTK001,161,3*36"  // Not needed currently
-#define PMTK_AWAKE "$PMTK010,002*2D"
+#define PMTK_STANDBY "$PMTK161,0*28\r\n"
+#define PMTK_STANDBY_SUCCESS "$PMTK001,161,3*36\r\n"  // Not needed currently
+#define PMTK_AWAKE "$PMTK010,002*2D\r\n"
 
 // ask for the release and version
-#define PMTK_Q_RELEASE "$PMTK605*31"
+#define PMTK_Q_RELEASE "$PMTK605*31\r\n"
 
 // request for updates on antenna status
-#define PGCMD_ANTENNA "$PGCMD,33,1*6C"
-#define PGCMD_NOANTENNA "$PGCMD,33,0*6D"
+#define PGCMD_ANTENNA "$PGCMD,33,1*6C\r\n"
+#define PGCMD_NOANTENNA "$PGCMD,33,0*6D\r\n"
 
 // ------- end of Adafruit defines
 
@@ -93,6 +94,8 @@ static struct minmea_float _track_true = {0,0};
 static struct minmea_float _gs_knots = {0,0};
 static struct minmea_float _mag_variation = {0,0};
 static struct minmea_float _altitude = {0,0};
+static struct minmea_date _current_date = {-1, -1, -1};
+static struct minmea_time _current_time = {-1, -1, -1, -1};
 static int _num_sats = -1;
 
 static char _altitude_units = '*';
@@ -108,14 +111,15 @@ static uint8_t _interpret_nmea(char *sentence)
 	case MINMEA_SENTENCE_RMC:
 	{
 		struct minmea_sentence_rmc rmc_sentence;
-		if (!minmea_parse_rmc(&rmc_sentence, sentence))
+		if (!minmea_parse_rmc(&rmc_sentence, sentence) || !rmc_sentence.valid)
 			return 0;
 		_latitude = rmc_sentence.latitude;
 		_longitude = rmc_sentence.longitude;
 		_gs_knots = rmc_sentence.speed;
 		_mag_variation = rmc_sentence.variation;
 		_track_true = rmc_sentence.course;
-		// TODO: get date/time
+		_current_date = rmc_sentence.date;
+		_current_time = rmc_sentence.time;
 		break;
 	}
 	case MINMEA_SENTENCE_GGA:
@@ -127,6 +131,8 @@ static uint8_t _interpret_nmea(char *sentence)
 		_longitude = gga_sentence.longitude;
 		_altitude = gga_sentence.altitude;
 		_altitude_units = gga_sentence.altitude_units;
+		_num_sats = gga_sentence.satellites_tracked;
+		_current_time = gga_sentence.time;
 		break;
 	}
 	case MINMEA_SENTENCE_GSA:
@@ -142,10 +148,11 @@ static uint8_t _interpret_nmea(char *sentence)
 		return 0;
 	case MINMEA_SENTENCE_GSV:
 	{
+		/*
 		struct minmea_sentence_gsv gsv_sentence;
 		if (!minmea_parse_gsv(&gsv_sentence, sentence))
 			return 0;
-		_num_sats = gsv_sentence.total_sats;
+		_num_sats = gsv_sentence.total_sats;*/
 		break;
 	}
 	case MINMEA_SENTENCE_VTG:
@@ -214,7 +221,10 @@ static uint8_t _gps_send_command(char *cmd, uint8_t len)
 		return 0;
 
 	memcpy(_tx_buf,cmd,len);
+	DMA_Cmd(DMA2_Stream7, DISABLE);
 	DMA_SetCurrDataCounter(DMA2_Stream7, len);
+	DMA_ClearFlag(DMA2_Stream7,DMA_FLAG_TCIF7);
+	DMA_Cmd(DMA2_Stream7, ENABLE);
 	_is_transmitting = 1;
 	USART_ClearFlag(USART1, USART_FLAG_TC);
 	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
@@ -269,7 +279,6 @@ uint8_t GPS_Initialize()
 	DMAStruct.DMA_Priority = DMA_Priority_High;
 	DMAStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
 	DMA_Init(DMA2_Stream7, &DMAStruct);
-	DMA_Cmd(DMA2_Stream7, ENABLE);
 
 	USART_ClearFlag(USART1, USART_FLAG_TC);
 	USART_ITConfig(USART1, USART_IT_TC, ENABLE);
@@ -289,44 +298,54 @@ uint8_t GPS_GetLastNMEA(char *buf)
 
 uint8_t GPS_CheckForNewData()
 {
-	if (xQueueReceive(_rx_queue, _sentence, 0) == pdPASS)
+	uint8_t ret = 0;
+	while (xQueueReceive(_rx_queue, _sentence, 0) == pdPASS)
 	{
+		ret = 1;
 		_interpret_nmea(_sentence);
-		return 1;
 	}
 
-	return 0;
+	return ret;
 }
 
 uint8_t GPS_GetCoords(float *latitude, float *longitude)
 {
-	if (latitude == NULL || longitude == NULL)
+	if (latitude == NULL && longitude == NULL)
 		return 0;
 
-	*latitude = minmea_tocoord(&_latitude);
-	*longitude = minmea_tocoord(&_longitude);
+	if (latitude != NULL)
+		*latitude = minmea_tocoord(&_latitude);
+
+	if (longitude != NULL)
+		*longitude = minmea_tocoord(&_longitude);
 
 	return 1;
 }
 
 uint8_t GPS_GetHeading(float *true_heading, float *mag_heading)
 {
-	if (true_heading == NULL || mag_heading == NULL)
+	if (true_heading == NULL && mag_heading == NULL)
 		return 0;
 
-	*true_heading = minmea_tofloat(&_track_true);
-	*mag_heading = minmea_tofloat(&_track_true) + minmea_tofloat(&_mag_variation);
+	if (true_heading != NULL)
+		*true_heading = minmea_tofloat(&_track_true);
+
+	if (mag_heading != NULL)
+		*mag_heading = minmea_tofloat(&_track_true) + minmea_tofloat(&_mag_variation);
 
 	return 1;
 }
 
 uint8_t GPS_GetAltitude(float *altitude, char *altitude_units)
 {
-	if (altitude == NULL || altitude_units == NULL)
+	if (altitude == NULL && altitude_units == NULL)
 		return 0;
 
-	*altitude = minmea_tofloat(&_altitude);
-	*altitude_units = _altitude_units;
+	if (altitude != NULL)
+		*altitude = minmea_tofloat(&_altitude);
+
+	if (altitude_units != NULL)
+		*altitude_units = _altitude_units;
 
 	return 1;
 }
@@ -349,4 +368,38 @@ uint8_t GPS_GetNumSats(int *num_sats)
 	*num_sats = _num_sats;
 
 	return 1;
+}
+
+uint8_t GPS_GetDateTime(int *year, int *month, int *day, int *hours, int *minutes, int *seconds)
+{
+	if (year != NULL)
+		*year = _current_date.year;
+
+	if (month != NULL)
+		*month = _current_date.month;
+
+	if (day != NULL)
+		*day = _current_date.day;
+
+	if (hours != NULL)
+		*hours = _current_time.hours;
+
+	if (minutes != NULL)
+		*minutes = _current_time.minutes;
+
+	if (seconds != NULL)
+		*seconds = _current_time.seconds;
+
+	return 1;
+}
+
+uint8_t GPS_GetUNIXTimestamp(struct timespec *ts)
+{
+	if (ts == NULL)
+		return 0;
+
+	if (minmea_gettime(ts, &_current_date, &_current_time) == 0)
+		return 1;
+
+	return 0;
 }
